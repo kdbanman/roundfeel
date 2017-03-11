e95eda77fdd030d0a37f2479bf47f5c3201a1840
Kirby Banman <kirby.banman@gmail.com>
2017-03-11 10:35:08 -0700

    Make pickups collectable

---
+++ Assets/Scripts/PickupCollector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupCollector : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Collectable") {
+            Destroy(other.gameObject);
+        }
+    }
+}
