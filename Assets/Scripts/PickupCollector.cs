a8a6331fd25c2abb9d36fd9b0adebcb18aeb023b
Kirby Banman <kirby.banman@gmail.com>
2017-03-11 16:55:26 -0700

    Move to unity 5.5, add win text

--- Assets/Scripts/PickupCollector.cs
+++ Assets/Scripts/PickupCollector.cs
@@ -1,11 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PickupCollector : MonoBehaviour {
+
+    public int score;
+    public Text scoreText;
+    public Text winText;
+
+    void Start() {
+        score = 0;
+        SetScore();
+        winText.text = "";
+    }
+
     void OnTriggerEnter(Collider other) {
         var collectable = other.gameObject.GetComponent<Collectable>();
         if (collectable != null) {
-            collectable.Collect();
+            score += collectable.Collect();
+            SetScore();
         }
+
+            winText.text = "";
+        if (score >= 1000 && score <= 1200) {
+            winText.text = "You fucking did it.";
+        }
+        else if (score >= 2000 && score <= 2200) {
+            winText.text = "You fucking did it again.";
+        }
+        else if (score >= 3000) {
+            winText.text = "Do you feel better now?";
+        }
+    }
+
+    void SetScore() {
+        scoreText.text = score.ToString();
     }
 }
