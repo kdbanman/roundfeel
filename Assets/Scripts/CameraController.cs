a9ad9dca8d2a1c392cf82d07ad0440729ddc47d7
Kirby Banman <kirby.b@getjobber.com>
2017-03-31 20:13:27 -0600

    Move camera in and give player a material

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+	public float offsetScale;
+
     private Vector3 offset;
     private Vector3 lastPlayerPosition;
 
@@ -13,7 +15,7 @@ public class CameraController : MonoBehaviour {
 
     void LateUpdate () {
         var playerPositionDelta = player.transform.position - lastPlayerPosition;
-        playerPositionDelta.Scale(new Vector3(0.1f, 0, 0.1f));
+		playerPositionDelta.Scale(new Vector3(offsetScale, 0, offsetScale));
 
         transform.position = transform.position + playerPositionDelta;
 
